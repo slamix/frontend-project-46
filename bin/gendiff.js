410da5d4f65789928d533099a544bdec738c2c15
Сергей Баев <lokomotivsapsan@gmail.com>
2024-03-18 21:09:13 +0300

    step 1: add performing file gendiff.js add packacge.json files, Makefile, gitignore eslint and entry point index.js

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse();
