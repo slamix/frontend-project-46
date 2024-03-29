3fe61d2969dd76a32f8c476967f21a98ba155abc
Сергей Баев <lokomotivsapsan@gmail.com>
2024-03-29 13:20:53 +0300

    edit parse function

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import path from 'node:path';
 const parseFiles = (file1, file2) => {
   const filepath1 = path.resolve(process.cwd(), file1);
   const filepath2 = path.resolve(process.cwd(), file2);
-  const dataFile1 = JSON.stringify(JSON.parse(fs.readFileSync(filepath1, 'utf-8')), null, '  ');
-  const dataFile2 = JSON.stringify(JSON.parse(fs.readFileSync(filepath2, 'utf-8')), null, '  ');
+  const dataFile1 = JSON.parse(fs.readFileSync(filepath1, 'utf-8'));
+  const dataFile2 = JSON.parse(fs.readFileSync(filepath2, 'utf-8'));
   return [dataFile1, dataFile2];
 };
 export default parseFiles;
